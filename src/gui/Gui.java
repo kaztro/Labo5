0b65a813752c9eea03b25565300eb3a01a09c3c7
kaztro <00229017@uca.edu.sv>
2018-06-19 00:44:50 -0600

    Plz :'v

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
 import java.awt.Container;
@@ -14,15 +9,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import threads.AnimalThread;
 
-/**
- *
- * @author kaztro
- */
 public class Gui extends JFrame{
     private JLabel [] labels;
-    private JButton inicio;
-    private String[] nombres = {"canguro", "tortuga", "dragon"};
-    
+
+    private JButton inicio,restart;
+
+    private String [] nombres = {"Canguro","tortuga","dragon"};
+
     public Gui(){
         super("Carrera de animales");
         initialComponents();
@@ -31,44 +24,63 @@ public class Gui extends JFrame{
     private void initialComponents() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-        
+
         labels = new JLabel[3];
-        inicio = new JButton("inicio");
+        inicio = new JButton("Inicio");
+        restart = new JButton("restart");
         Container container = getContentPane();
-        
-        for (int i = 0; i < 3; i++) {
+
+        for(int i=0;i<3;i++){
             labels[i] = new JLabel();
-            labels[i].setIcon(new ImageIcon(getClass().getResource(nombres[i] + ".gif")));
-            labels[i].setBounds(10, (i*220) + 10,200 ,200);
+            labels[i].setIcon(new ImageIcon(getClass().getResource(nombres[i]+".gif")));
+            labels[i].setBounds(10,(i*220)+10,200,200);
             container.add(labels[i]);
         }
-        
-        inicio.setBounds(10, 0, 100, 30);
+        inicio.setBounds(10,0,100,30);
         container.add(inicio);
-        
-        inicio.addActionListener(new ActionListener(){
+
+        restart.setBounds(100,0,100,30);
+        container.add(restart);
+        restart.setVisible(false);
+
+        inicio.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                AnimalThread canguro;
-                canguro = new AnimalThread("canguro", 510, labels[0], labels[0].getX()+30, labels[0].getY());
-                AnimalThread tortuga;
-                tortuga = new AnimalThread("tortuga", 510, labels[1],labels[1].getX()-100, labels[1].getY());
-                AnimalThread dragon;
-                dragon = new AnimalThread("dragon", 510, labels[2],labels[2].getX()+60, labels[2].getY());
+            public void actionPerformed(ActionEvent e){
+                AnimalThread canguro = new AnimalThread("canguro",labels[0].getX()+30,labels[0].getY(),510,labels[0]);
+                AnimalThread tortuga = new AnimalThread("tortuga",labels[1].getX()-100,labels[1].getY(),510,labels[1]);
+                AnimalThread dragon = new AnimalThread("dragon",labels[2].getX()+60,labels[2].getY(),510,labels[2]);
                 canguro.start();
                 tortuga.start();
                 dragon.start();
+                inicio.setVisible(false);
+                restart.setVisible(true);
+            }
+
+        });
+        restart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                for(int i=0;i<3;i++){
+                    labels[i].setLocation(10,(i*220));
+                    inicio.setVisible(true);
+                    restart.setVisible(false);
+                }
+            }
+
+        });
+        setSize(700,650);
+
+    }
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run(){
+                new Gui().setVisible(true);
             }
         });
-        setSize(700, 650);
-    }
-    public static void main(String[] args) {    
-   java.awt.EventQueue.invokeLater(new Runnable(){
-       @Override
-       public void run() {
-            new Gui().setVisible(true);
-       }
-   });
-}
-}
 
+    }
+
+
+}
--- src/threads/AnimalThread.java
+++ src/threads/AnimalThread.java
@@ -1,51 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package threads;
 
 import javax.swing.JLabel;
-
-/**
- *
- * @author kaztro
- */
 public class AnimalThread extends Thread{
     private String nombre;
     private int limite;
     private JLabel animal;
     private int x;
     private int y;
-    private int limiteregre;
-    
+
     public AnimalThread(){}
 
-    public AnimalThread(String nombre, int limite, JLabel animal, int x, int y) {
+    public AnimalThread(String nombre, int x, int y, int limite, JLabel animal){
         this.nombre = nombre;
-        this.limite = limite;
-        this.animal = animal;
         this.x = x;
         this.y = y;
+        this.limite = limite;
+        this.animal = animal;
     }
 
     @Override
-    public void run() {
-        for ( int i = x ; i <= this.limite ; i += 10){
+    public void run(){
+        for (int i=x; i<=this.limite;i+=10){
             System.out.println(this.nombre + " avanza");
-            this.animal.setLocation(i, y);
+            this.animal.setLocation(i,y);
             try{
                 sleep(100);
-            } catch (InterruptedException e){
+            }catch( InterruptedException e){
                 e.printStackTrace();
             }
         }
-        System.out.println(this.nombre + "a llegado a la meta");
-        
+        System.out.println(this.nombre + " a llegado a la meta");
         yield();
     }
-    
-    
-    
-    
 }
+
